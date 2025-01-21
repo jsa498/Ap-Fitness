0c14f2422332c2d83b605dbb6405d782f49e4b01
jsa498 <jaskaran1431085@gmail.com>
2025-01-21 00:25:12 -0800

    Fix: Remove use client directives from pages to allow metadata export

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { motion } from 'framer-motion';
 import { FaDumbbell, FaUsers, FaHeart, FaAppleAlt, FaLaptop } from 'react-icons/fa';
 import { GiBoxingGlove } from 'react-icons/gi';
